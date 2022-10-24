8a63f98ffba44e1881c4da836fa0fb76b52b2bc7
tallys-aureliano <tallysaureliano@outlook.com>
2022-10-24 09:28:19 -0300

    testando operadores

---
+++ Modulo-2/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+        int a, b;
+        a = 2;
+        b = 3;
+
+        String result = a==b ? "Verdadeiro" : "Falso";
+
+        System.out.println(result);
+
+
+
+    }
+}
