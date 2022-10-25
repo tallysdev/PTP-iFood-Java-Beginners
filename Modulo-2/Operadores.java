79ea64329eb6082d835e829da9293777ff260a43
tallys-aureliano <tallysaureliano@outlook.com>
2022-10-25 20:37:02 -0300

    testando operadores e objetos

--- Modulo-2/Operadores.java
+++ Modulo-2/Operadores.java
@@ -2,6 +2,11 @@ public class Operadores {
     public static void main(String[] args) {
         
         int a, b;
+        String nome, nome2;
+        nome2 = "tallys";
+        nome = new String ("tallys");
+        System.out.println(nome2.equals(nome));
+
         a = 2;
         b = 3;
 
