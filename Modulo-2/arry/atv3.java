e1c0d42cc23adeb8234a9472457cc0590ac4b178
tallys-aureliano <tallysaureliano@outlook.com>
2022-11-06 17:03:20 -0300

    Atividade 3

---
+++ Modulo-2/arry/atv3.java
@@ -0,0 +1,27 @@
+package arry;
+
+import java.util.Random;
+
+import javax.swing.BoundedRangeModel;
+
+class atv3 {
+    
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+
+        int[] numeroAleatorios = new int[20];
+
+        for (int i = 0; i < numeroAleatorios.length; i++) {
+            numeroAleatorios[i] = random.nextInt(100);
+        }
+        
+        for (int i : numeroAleatorios) {
+            System.out.print(" " + i);
+        }
+        System.out.println("\n");
+        for (int i : numeroAleatorios) {
+            System.out.print(" " + (i + 1));
+        }
+    }
+}
