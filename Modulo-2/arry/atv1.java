00d0ff04d67f31afe140c6ad028e98b44767264f
tallys-aureliano <tallysaureliano@outlook.com>
2022-11-06 16:04:38 -0300

    deixando mais dinamico

--- Modulo-2/arry/atv1.java
+++ Modulo-2/arry/atv1.java
@@ -8,6 +8,13 @@ class atv1 {
         Scanner sc = new Scanner(System.in);
 
         int[] vetor = {11,34,-76,32,5};
+        
+        System.out.println("Ordem original");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
+
+        System.out.println("\nOrdem inversa");
         for (int i = vetor.length-1; i >= 0; i--) {
             System.out.println(vetor[i]);
         }
