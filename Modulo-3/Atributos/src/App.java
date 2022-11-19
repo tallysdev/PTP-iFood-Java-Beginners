10fb1ee6ea7e81555ba4260f85f812e99cc7cde6
tallys-aureliano <tallysaureliano@outlook.com>
2022-11-19 16:47:50 -0300

    Atributos e metodos

--- Modulo-3/Atributos/src/App.java
+++ Modulo-3/Atributos/src/App.java
@@ -4,8 +4,23 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        Vendas.pagamento(150);
-        Vendas.pagamento(150, 25);
-        Vendas.pagamento(150,5,1);
+        Vendas sapato = new Vendas();
+        sapato.setData("dd/mm/aaaa");
+        sapato.setDesconto(20);
+        sapato.setJuros(2);
+        sapato.setParcelas(8);
+        sapato.setPedido("sapato social");
+        sapato.setValorPeca(150);
+
+        System.out.println(sapato.getData());
+        System.out.println(sapato.getDesconto());
+        System.out.println(sapato.getJuros());
+        System.out.println(sapato.getParcelas());
+        System.out.println(sapato.getPedido());
+        System.out.println(sapato.getValorPeca());
+        
+        Vendas.pagamento(sapato.valorPeca);
+        Vendas.pagamento(sapato.valorPeca, sapato.desconto);
+        Vendas.pagamento(sapato.valorPeca,sapato.parcelas,sapato.juros);
     }
 }
--- Modulo-3/Atributos/src/Vendas.java
+++ Modulo-3/Atributos/src/Vendas.java
@@ -2,12 +2,11 @@ class Vendas {
     int valorPeca, desconto, parcelas, juros;
     String pedido, data;
 
-
-    Vendas(){
+    Vendas() {
 
     }
 
-    Vendas(int valorPeca, int desconto, int parcelas, int juros, String pedido, String data){
+    Vendas(int valorPeca, int desconto, int parcelas, int juros, String pedido, String data) {
 
         this.valorPeca = valorPeca;
         this.desconto = desconto;
@@ -18,37 +17,71 @@ class Vendas {
 
     }
 
-    void setPedido(String pedido){
+    // SET'S
+    void setPedido(String pedido) {
         this.pedido = pedido;
     }
 
-    void setValorPeca(int valorPeca){
+    void setValorPeca(int valorPeca) {
         this.valorPeca = valorPeca;
+    }
+
+    public void setDesconto(int desconto) {
+        this.desconto = desconto;
+    }
+
+    public void setParcelas(int parcelas) {
+        this.parcelas = parcelas;
+    }
+
+    public void setJuros(int juros) {
+        this.juros = juros;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    // GET'S
+    public int getJuros() {
+        return juros;
+    }
+
+    public int getParcelas() {
+        return parcelas;
+    }
+
+    public int getDesconto() {
+        return desconto;
+    }
+
+    public int getValorPeca() {
+        return valorPeca;
+    }
+    
+    public String getData() {
+        return data;
     }
 
     public String getPedido() {
         return pedido;
     }
 
-    public int getValorPeca() {
-        return valorPeca;
-    }
 
-    public static void pagamento(int valorPeca){
+    public static void pagamento(int valorPeca) {
 
         System.out.println("O valor final da compra é " + valorPeca);
 
     }
 
-    public static void pagamento(int valorPeca, int desconto){
+    public static void pagamento(int valorPeca, int desconto) {
 
-        System.out.println("O valor final da peça é " + (valorPeca-desconto));
-    
+        System.out.println("O valor final da peça é " + (valorPeca - desconto));
+
     }
 
-    public static void pagamento(int valorPeca, int parcelas, int juros){
+    public static void pagamento(int valorPeca, int parcelas, int juros) {
 
-        System.out.println("O valor final da peça é " + (((valorPeca/parcelas)*juros)*parcelas) + " em " + parcelas);
+        System.out.println("O valor final da peça é " + (((valorPeca / parcelas) * juros) * parcelas) + " em " + parcelas);
 
     }
 
