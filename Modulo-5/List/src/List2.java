7eec272521d447f34f7232f9ba816731c30f734a
tallys-aureliano <tallysaureliano@outlook.com>
2022-12-03 14:09:07 -0300

    ordenando por Nome, Cor e Idade

--- Modulo-5/List/src/List2.java
+++ Modulo-5/List/src/List2.java
@@ -12,6 +12,7 @@ class List2 {
     
     List<Gato> meusGatos = new ArrayList<Gato>(){{
         add(new Gato("tata", "preto", 8));
+        add(new Gato("tata", "azul", 4));
         add(new Gato("fafa", "braco", 9));
         add(new Gato("yaya", "lilas", 5));
     }};    
@@ -30,6 +31,11 @@ class List2 {
     System.out.println("Ordenando por Cor");
     // Collections.sort(meusGatos, new ComparatorIdade());
     meusGatos.sort(new ComparatorCor());
+    System.out.println(meusGatos);
+
+    System.out.println("Ordenando por Nome/Cor/Idade");
+    // Collections.sort(meusGatos, new ComparatorIdade());
+    meusGatos.sort(new ComparatorNomeCorIdade());
     System.out.println(meusGatos);
 
     }
@@ -114,6 +120,26 @@ class ComparatorCor implements Comparator<Gato> {
     public int compare(Gato g1, Gato g2) {
         // TODO Auto-generated method stub
         return g1.getCor().compareToIgnoreCase(g2.getCor());
+    }
+    
+}
+
+class ComparatorNomeCorIdade implements Comparator<Gato> {
+
+    /* (non-Javadoc)
+     * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+     */
+    @Override
+    public int compare(Gato g1, Gato g2) {
+
+        int nome = g1.getNome().compareToIgnoreCase(g2.getNome());
+        if (nome !=0) return nome;
+
+        int cor = g1.getCor().compareToIgnoreCase(g2.getCor());
+        if (cor!=0) return cor;
+
+
+        return Integer.compare(g1.getIdade(), g2.getIdade()) ;
     }
     
 }
