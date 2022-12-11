e28e4c6ea86ea50fe0e0182f74c9ee8274cc2ef6
tallys-aureliano <tallysaureliano@outlook.com>
2022-12-11 15:10:43 -0300

    Exercicios propostos

---
+++ Modulo-5/Set/Exercicio1.java
@@ -0,0 +1,30 @@
+import java.rmi.server.SocketSecurityException;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+class Exercicio1 {
+    
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+
+        Set<String> arcoiris = new LinkedHashSet<String>() {{ 
+            add("vermelho");
+            add("laranja");
+            add("amarelo");
+            add("verde");
+            add("azul");
+            add("anil");
+            add("violeta");
+        }};
+        System.out.println("Letra A");
+        for (String string : arcoiris) {
+            System.out.println(string);
+        }
+        System.out.println("Letra B");
+        System.out.println(arcoiris.size());
+    }
+}
