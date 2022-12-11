259348b5140353430feaee24df7773cb8249889c
tallys-aureliano <tallysaureliano@outlook.com>
2022-12-11 13:34:37 -0300

    novas formas de ordenação

--- Modulo-5/Set/Ordenacao.java
+++ Modulo-5/Set/Ordenacao.java
@@ -1,6 +1,7 @@
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 class Ordenacao {
     
@@ -15,13 +16,12 @@ class Ordenacao {
         // Serie: Wandinha, genero: fantasia, ep:8
 
 
-        System.out.println("Ordem aleatoria");
+        System.out.println("Ordem aleatoria\n");
         Set<Serie> minhasSeries = new HashSet<Serie>(){{
             add(new Serie("got","fantasia",60));
             add(new Serie("Sandman","fantasia",10));
             add(new Serie("Wandinha","fantasia",8));
         }};
-        System.out.println();
         for (Serie serie : minhasSeries) {
             System.out.println(serie.getNome());
             System.out.println(serie.getGenero());
@@ -30,13 +30,32 @@ class Ordenacao {
         }
 
         Set<Serie> minhaSeries2 = new LinkedHashSet<Serie>(){{
-            add(new Serie("got","fantasia",60));
+            add(new Serie("got","fantasia",3));
             add(new Serie("Sandman","fantasia",10));
             add(new Serie("Wandinha","fantasia",8));
         }};
-        System.out.println("Ordem por add");
-        System.out.println();
+        System.out.println("Ordem por add\n");
         for (Serie serie : minhaSeries2) {
+            System.out.println(serie.getNome());
+            System.out.println(serie.getGenero());
+            System.out.println(serie.getEp());
+            System.out.println(" ");
+        }
+
+        System.out.println("Por quantidad de episodios com criterio de desenpate o nome ");
+        Set<Serie> minhaSeries3 = new TreeSet<Serie>(minhaSeries2);
+        System.out.println();
+        for (Serie serie : minhaSeries3) {
+            System.out.println(serie.getNome());
+            System.out.println(serie.getGenero());
+            System.out.println(serie.getEp());
+            System.out.println(" ");
+        }
+
+        System.out.println("Comparando por nome\n");
+        Set<Serie> minhaSeries4 = new TreeSet<>(new ComparatorNomeGenerpEp());
+        minhaSeries4.addAll(minhaSeries3);
+        for (Serie serie : minhaSeries4) {
             System.out.println(serie.getNome());
             System.out.println(serie.getGenero());
             System.out.println(serie.getEp());
