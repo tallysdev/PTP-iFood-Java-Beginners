f76d7e57e8e5df6473958c07cdc58b513464d234
tallys-aureliano <tallysaureliano@outlook.com>
2022-12-11 15:46:42 -0300

    exercicios propostos

--- Modulo-5/Set/Exercicio1.java
+++ Modulo-5/Set/Exercicio1.java
@@ -1,6 +1,12 @@
-import java.util.LinkedHashSet;
 import java.util.Set;
-
+import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
 class Exercicio1 {
     
 
@@ -25,5 +31,38 @@ class Exercicio1 {
         }
         System.out.println("Letra B");
         System.out.println(arcoiris.size());
+        
+        System.out.println("Letra C");
+        Set<String> arcoiris2 = new TreeSet<>(arcoiris);
+        System.out.println(arcoiris2);
+
+        System.out.println("Letra D");
+        Set<String> arcoires3 = new LinkedHashSet<>(Arrays.asList("vermelho", "laranja", "amarelo", "verde", "azul", "anil", "violeta"));
+        System.out.println(arcoires3);
+        List<String> coresList = new ArrayList<>(arcoires3);
+        Collections.reverse(coresList);
+        System.out.println(coresList);
+
+        System.out.println("Letra E");
+        for (String string : arcoiris) {
+            if (string.startsWith("v")) System.out.println(string);
+        }
+
+        System.out.println("letra F ");
+        Iterator<String> iterator2 = arcoiris.iterator();
+        while (iterator2.hasNext()) {
+            if (!iterator2.next().startsWith("v")) iterator2.remove();
+        }
+        System.out.println(arcoiris);
+
+        System.out.println("Letra G");
+        arcoiris.clear();
+
+        System.out.println("Letra H " + arcoiris.isEmpty());
     }
+}
+
+class Ordenacao {
+
+
 }
