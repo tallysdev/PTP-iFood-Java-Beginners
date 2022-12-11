8502047178d857d78699fd2b431beb51285c26c1
tallys-aureliano <tallysaureliano@outlook.com>
2022-12-11 12:46:06 -0300

    forma aleatoria

--- Modulo-5/Set/Ordenacao.java
+++ Modulo-5/Set/Ordenacao.java
@@ -1,5 +1,11 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Ordenacao {
     
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         
         // Vamos ordenar series dentro de um conjunto
@@ -9,6 +15,17 @@ class Ordenacao {
 
 
         System.out.println("Ordem aleatoria");
-
+        Set<Serie> minhasSeries = new HashSet<Serie>(){{
+            add(new Serie("got","fantasia",60));
+            add(new Serie("Sandman","fantasia",10));
+            add(new Serie("Wandinha","fantasia",8));
+        }};
+        System.out.println();
+        for (Serie serie : minhasSeries) {
+            System.out.println(serie.getNome());
+            System.out.println(serie.getGenero());
+            System.out.println(serie.getEp());
+            System.out.println(" ");       
+        }
     }
 }
