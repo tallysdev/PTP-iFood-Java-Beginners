0ad1ccd3d739062655156cc9f9f4c3ff359ee101
tallys-aureliano <tallysaureliano@outlook.com>
2022-12-11 12:35:14 -0300

    Ex de ordenação

---
+++ Modulo-5/Set/Ordenacao.java
@@ -0,0 +1,14 @@
+class Ordenacao {
+    
+    public static void main(String[] args) {
+        
+        // Vamos ordenar series dentro de um conjunto
+        // Serie: got, genero: fantisa, ep: 60
+        // Serie:Sandman, genero: fantasia, ep: 8
+        // Serie: Wandinha, genero: fantasia, ep:8
+
+
+        System.out.println("Ordem aleatoria");
+
+    }
+}
