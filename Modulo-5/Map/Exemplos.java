591459ecfdb06a2a219723f150154e83aad4af11
tallys-aureliano <tallysaureliano@outlook.com>
2022-12-11 16:41:34 -0300

    add examples

--- Modulo-5/Map/Exemplos.java
+++ Modulo-5/Map/Exemplos.java
@@ -1,8 +1,48 @@
+import java.util.Map;
+import java.util.Set;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+
 class Exemplos {
     
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         
-        
+        Map<String, Double> carrosp = new HashMap<String, Double>(){{
+            put("F gol", 12d);
+            put("savero", 11d);
+            put("uno", 10d);
+            put("mobi", 9d);
+            put("pampa", 8d);
+        }};
+        System.out.println(carrosp);
 
+        carrosp.put("F gol", 11d);
+        System.out.println(carrosp);
+
+        System.out.println("Vendo se tem algum carro com o nome d20 " +carrosp.containsKey("d20"));
+
+        System.out.println("Consumo do  uno " + carrosp.get("uno"));
+
+        Set<String> modelos = carrosp.keySet();
+        System.out.println(modelos);
+
+        Collection<Double> consumos = carrosp.values();
+        System.out.println(consumos);
+
+        Double eco = Collections.max(carrosp.values());
+        Set<Map.Entry<String, Double>> entries =carrosp.entrySet();   
+        System.out.println(entries);
+        String modelo2 = "";
+        for (Map.Entry<String,Double> entry : entries) {
+            if (entry.getValue().equals(eco)) {
+                modelo2 = entry.getKey();
+                System.out.println(modelo2 + " " + eco);
+            }
+        }
+        
     }
 }
